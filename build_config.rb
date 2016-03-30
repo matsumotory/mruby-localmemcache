49f16c14869e6cd32e567e53f513971b47957d39
matsumoto-r <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2016-03-30 16:57:31 +0900

    Don't use mruby-sleep

--- build_config.rb
+++ build_config.rb
@@ -4,7 +4,6 @@ MRuby::Build.new do |conf|
   # include the default GEMs
   conf.gembox 'full-core'
   conf.gem File.expand_path(File.dirname(__FILE__))
-  conf.gem :github => 'matsumoto-r/mruby-sleep'
   conf.enable_test
 end
 
